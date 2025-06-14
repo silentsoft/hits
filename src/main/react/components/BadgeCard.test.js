a9e870c9906a4a685044b1db3237f26877caf1c8
silentsoft <hyesung.lee@silentsoft.org>
2025-06-14 17:14:54 +0900

    Fix BadgeCard test failure

--- src/main/react/components/BadgeCard.test.js
+++ src/main/react/components/BadgeCard.test.js
@@ -2,12 +2,15 @@ import {render, screen} from "@testing-library/react";
 import BadgeCard from "./BadgeCard";
 import userEvent from "@testing-library/user-event";
 import {act} from "react-dom/test-utils";
-import copy from "copy-text-to-clipboard";
-
-jest.mock("copy-text-to-clipboard");
 
 describe("BadgeCard", () => {
-    it("should be displayed the success icon after copy", () => {
+    it("should be displayed the success icon after copy", async () => {
+        Object.assign(navigator, {
+            clipboard: {
+                writeText: jest.fn().mockResolvedValue(undefined)
+            }
+        })
+
         jest.useFakeTimers();
 
         render(<BadgeCard title="foo" render={() => "bar"} />);
@@ -18,7 +21,9 @@ describe("BadgeCard", () => {
         expect(screen.queryByTitle("Copy")).toBeInTheDocument();
         expect(screen.queryByTitle("Copied!")).not.toBeInTheDocument();
 
-        userEvent.click(screen.getByTitle("Copy"));
+        await userEvent.click(screen.getByTitle("Copy"));
+
+        expect(navigator.clipboard.writeText).toHaveBeenCalledWith("bar");
 
         expect(screen.queryByTitle("Copy")).not.toBeInTheDocument();
         expect(screen.queryByTitle("Copied!")).toBeInTheDocument();
@@ -30,15 +35,24 @@ describe("BadgeCard", () => {
         expect(screen.queryByTitle("Copy")).toBeInTheDocument();
         expect(screen.queryByTitle("Copied!")).not.toBeInTheDocument();
     });
-    it("should not be displayed the success icon when an error occurs", () => {
-        copy.mockImplementation(() => {
-            throw new Error();
-        });
+    it("should fallback to execCommand when clipboard API is not available", async () => {
+        delete navigator.clipboard;
+
+        document.execCommand = jest.fn();
 
         render(<BadgeCard title="foo" render={() => "bar"} />);
 
+        await userEvent.click(screen.getByTitle("Copy"));
+
+        expect(document.execCommand).toHaveBeenCalledWith("copy");
+
+        expect(await screen.findByTitle("Copied!")).toBeInTheDocument();
+
+        act(() => {
+            jest.advanceTimersByTime(1500);
+        });
+
         expect(screen.queryByTitle("Copy")).toBeInTheDocument();
-        userEvent.click(screen.getByTitle("Copy"));
         expect(screen.queryByTitle("Copied!")).not.toBeInTheDocument();
     });
 });
